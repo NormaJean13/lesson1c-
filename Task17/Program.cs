5c763cbd142d772fba36c6cbb026dd803685dfb5
NormaJean13 <navladimirovna@vk.com>
2022-09-06 22:21:29 +0300

    practic0609

---
+++ Task17/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты точки ");
+Console.Write ("x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write ("y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc, int yc)
+{
+if(xc > 0 && yc > 0) return "Диапазон соответствует первой четверти";
+if(xc < 0 && yc > 0) return "Диапазон соответствует второй четверти";
+if(xc < 0 && yc < 0) return "Диапазон соответствует третьей четверти";
+if(xc > 0 && yc < 0) return "Диапазон соответствует четвертой четверти"; 
+return "Введены некорректные координаты";
+}
+string result = Quarter (x, y);
+Console.WriteLine(result);
---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти ");
+Console.Write ("quarter: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+
+
+string Point(int quarter)
+{
+if(quarter == 1) return "X > 0, Y > 0";
+if(quarter == 2) return "X > 0, Y < 0";
+if(quarter == 3) return "X < 0, Y < 0";
+if(quarter == 4) return "X < 0, Y > 0"; 
+return "Введен некорректный номер четверти";
+}
+string result = Point (quarter);
+Console.WriteLine(result);
